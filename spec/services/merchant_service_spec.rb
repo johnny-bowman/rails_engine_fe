8f60ef842ddc37e12ffc9960c8a356330a3d04cd
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-26 14:42:21 -0600

    Test: merchant service gets merchant items

--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -13,10 +13,24 @@ RSpec.describe 'merchant service' do
 
   it 'gets a single merchant' do
     search = MerchantService.merchant("1")
-    # binding.pry
+
     expect(search).to be_a Hash
     expect(search[:data]).to be_a Hash
     expect(search[:data]).to have_key :id
     expect(search[:data][:attributes]).to have_key :name
+  end
+
+  it 'gets merchant items' do
+    search = MerchantService.items("1")
+
+    expect(search).to be_a Hash
+    expect(search[:data]).to be_a Array
+
+    item = search[:data].first
+    expect(item).to have_key :id
+    expect(item[:attributes]).to have_key :name
+    expect(item[:attributes]).to have_key :description
+    expect(item[:attributes]).to have_key :unit_price
+    expect(item[:attributes]).to have_key :merchant_id
   end
 end
