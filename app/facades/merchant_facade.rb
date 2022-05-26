9259982b7ed875eaf551747f928072b269b309a8
Johnny Bowman <johntbowman.production@gmail.com>
2022-05-26 14:33:24 -0600

    Facade: Merchant facade

---
+++ app/facades/merchant_facade.rb
@@ -0,0 +1,15 @@
+class MerchantFacade
+  def self.merchants
+    json = MerchantService.merchants
+
+    json[:data].map do |merchant_data|
+      Merchant.new(merchant_data)
+    end
+  end
+
+  def self.merchant(id)
+    json = MerchantService.merchant(id.to_s)
+
+    Merchant.new(json[:data])
+  end
+end
